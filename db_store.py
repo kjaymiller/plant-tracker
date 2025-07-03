24105c2c2fbaabbc5b601887a39b53ebed16f576
Jay Miller <kjaymiller@gmail.com>
2025-07-03 11:08:15 -0400

    use built-ins for data

--- db_store.py
+++ db_store.py
@@ -4,16 +4,12 @@ Get readings from grow hat
 
 import socket
 import os
-import time
 import datetime
 import json
 import logging
 
 import dotenv
 import psycopg
-from rich import print as rprint
-from rich.live import Live
-from rich.spinner import Spinner
 
 from grow.moisture import Moisture
 
@@ -23,39 +19,26 @@ dotenv.load_dotenv()
 # Set up logging
 logging.basicConfig(
     level=logging.WARNING,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%dT%H:%M:%S",
-    handlers=[
-        logging.FileHandler('grow_hat.log'),
-        logging.StreamHandler()
-    ]
+    handlers=[logging.FileHandler("grow_hat.log"), logging.StreamHandler()],
 )
 
 logger = logging.getLogger(__name__)
 
 connection = psycopg.connect(os.getenv("AIVEN_PG_CONNECTION_STRING"))
 
+sensors = {
+    "m1": Moisture(1),
+    "m2": Moisture(2),
+    "m3": Moisture(3),
+}
+
+
 def reading():
-    meter = [Moisture(_+1) for _ in range(3)]
-    spinner = Spinner(name="line", text="Loading Sensors") 
-    process_timer = datetime.datetime.now()
-
-    with Live(spinner.render(0.5), refresh_per_second=10) as live:
-        meters_online = [m.moisture for m in meter]
-
-        while not all(meters_online):
-            meters_online = [m.moisture for m in meter]
-            if (process_timer - datetime.datetime.now()).total_seconds() >= 3 and not all(meters_online):
-                msg = "Unable to process all nodes please check device"
-                logger.warning(msg)
-                break
     try:
         data = {
-            "sensors": {
-                "m1": meter[0].moisture,
-                "m2": meter[1].moisture,
-                "m3": meter[2].moisture,
-            },
+            "sensors": {k: v._reading for k, v in sensors.items() if v.active},
             "device": socket.gethostname(),
         }
 
@@ -64,14 +47,12 @@ def reading():
         return
 
         with connection.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 Insert INTO moisture_logs (datetime, moisture_data) 
                 VALUES (%s, %s)
                 """,
-                (
-                    datetime.datetime.now().isoformat(),
-                    dumped_data
-                ),
+                (datetime.datetime.now().isoformat(), dumped_data),
             )
             connection.commit()
     except Exception as e:
