1dd5bec03e37b8c4101bd13fbe9297d3f4a105cd
Jay Miller <kjaymiller@gmail.com>
2025-09-05 10:01:34 -0400

    testing settings for web deploy

--- plant_tracker/settings.py
+++ plant_tracker/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*.aiven.app"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
